07a8e5cd835f2946b6584bccc7f3650d045f09e0
Jayakrishna Surampudi <Jayakrishna.jk14@gmail.com>
2022-01-23 03:58:02 +0530

    modified text using button (using useState)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeading] = useState("Hello ");
+  const [paragraphText, changeText] = useState("HI THIS IS A PARAGRAPH");
+  function handleClick() {
+    setHeading(" Name submitted");
+  }
+  function handleClick2() {
+    changeText("hi i had modified paragraph using button and using useState");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
+      <p>{paragraphText}</p>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
+      <button type="submit" onMouseOver={handleClick2}>
+        change text
+      </button>
     </div>
   );
 }
